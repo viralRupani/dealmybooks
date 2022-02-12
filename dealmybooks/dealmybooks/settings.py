c2302703c2730ffb74542dfdefd8b3150c273f90
Viral <viralrupani12017@gmail.com>
2022-02-12 12:00:14 +0530

    ,

--- dealmybooks/dealmybooks/settings.py
+++ dealmybooks/dealmybooks/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-6h%jm3-k$nu=hb8^+ox*c8fxa3e(%@2=ir2vl60#vu2z#49qox"
 
 # SECURITY WARNING: don't run with debug turned on in production
-DEBUG = config('DEBUG', cast = bool, default=False)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dealmybooks',
-        'USER': config('user'),
+        'USER': 'root',
         'PASSWORD':"",
         'HOST': '127.0.0.1',
         'PORT': '3306',
